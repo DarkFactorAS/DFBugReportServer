c7162bb7d64b4b52e1200bde78333228601776fe
Thor Richard Hanssen <thor.richard.hanssen@gmail.com>
2022-04-14 14:56:44 +0200

    Moved server to a seperate folder

---
+++ Server/Controllers/BugReportController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using BugReportServer.Model;
+using BugReportServer.Provider;
+
+namespace BugReportServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BugReportController : ControllerBase
+    {
+        IBugReportProvider _provider;
+
+        public BugReportController(IBugReportProvider provider)
+        {
+            _provider = provider;
+        }
+
+        [HttpPut]
+        [Route("ReportBug")]
+        public BugReponseData ReportBug(BugReportData bugReportData)
+        {
+            return _provider.ReportBug(bugReportData);
+        }
+
+        [HttpPut]
+        [Route("AttachFile")]
+        public BugReponseFileData AttachFile(BugReportFileData fileData)
+        {
+            return _provider.AttachFile(fileData);
+        }
+    }
+}
